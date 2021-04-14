77cb9aceed57a06109e99e012e577c66963940bf
tequibo <tequibo@gmail.com>
2021-04-14 07:27:47 +0300

    124312

--- script.js
+++ script.js
@@ -121,22 +121,12 @@ function gameLoop(delta){
 }
 function onClick1() {
     texture=texture1;
-    let bunny = new PIXI.Sprite(texture);
-        bunny.targetScale=Math.random()*.3;
-        
-        bunny.anchor.x = 0.5;
-        bunny.anchor.y = 0.5;
-        bunny.scale.x=0;
-        bunny.scale.y=0;
-        bunny.rotation=Math.random()*6.28;
-        bunny.x=itr.mouse.global.x;//Math.random()*w;
-        bunny.y=itr.mouse.global.y;//Math.random()*h;
-        bs.push(bunny);
-
-        app.stage.addChild(bunny)
+    b1.scale.y=Math.random()+.5;
 }
 function onClick2() {
     texture=texture2;
+    b2.scale.x=Math.random()+.5;
+
 }
 function onClick3() {
     texture=texture3;
