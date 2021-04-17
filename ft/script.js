961cbc654de3f7934693dada8d8a608c3f1fc4c6
tequibo <tequibo@gmail.com>
2021-04-17 18:23:00 +0300

    🎈

--- ft/script.js
+++ ft/script.js
@@ -19,30 +19,33 @@ window.addEventListener("resize", resize);
 
 function resize() {
     w = window.innerWidth;
-    h = window.innerHeight;    
+    h = window.innerHeight;  
+renderer.setSize(window.innerWidth, window.innerHeight);
+
 }
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
+// const controls = new OrbitControls( camera, renderer.domElement );
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
 cubes =[];
 for (let i = 0; i < 30; i++) {
-    const geometry = new THREE.BoxGeometry();
+    // const geometry = new THREE.BoxGeometry();
+    const geometry = new THREE.ConeGeometry(.1,Math.random()*2);
     const material = new THREE.MeshBasicMaterial( { color: 0x00ffff*Math.random() } );
     
     const cube = new THREE.Mesh(geometry, material);
     cube.position.x=Math.random()*10-5;
-    cube.position.y=Math.random()*10-5;
+    cube.position.z=Math.random()*-5;
     cubes.push(cube)
     scene.add(cube);
 }
 
 camera.position.z = 5;
-
+// camera.position.set( 0, 20, 100 );
+// controls.update();
 function animate() {
 	requestAnimationFrame(animate);
     for (let i = 0; i < cubes.length; i++) {
@@ -52,7 +55,7 @@ function animate() {
         c.rotation.y += 0.00121*i;
         console.log(i);
     }
-    
+    // controls.update();
 	renderer.render( scene, camera );
 }
 animate();
