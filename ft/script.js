bd8d9719e5a2f3636e18b9f0c89aa3dd8432cbc8
tequibo <tequibo@gmail.com>
2021-04-19 18:45:25 +0300

    particles

--- ft/script.js
+++ ft/script.js
@@ -24,11 +24,11 @@ let container, stats;
 // stats = new Stats();
 // container.appendChild( stats.dom );
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(1,1,1);
+scene.background = new THREE.Color(0,0,0);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.y = 40;
-camera.position.z = 40;
+camera.position.y = 140;
+camera.position.z = 140;
 camera.lookAt(0,0,0);
 const p_light = new THREE.PointLight(0x00ffff, 2, 200);
 const p_light2 = new THREE.PointLight(0xff0000, 2, 200);
@@ -52,7 +52,6 @@ const rectLight2 = new THREE.DirectionalLight( 0x00ffff, 2 );
 rectLight2.position.set( 30 , 0, 0 );
 rectLight2.lookAt(0,0,0);
 // scene.add(rectLight2);
-const geoFloor = new THREE.BoxGeometry( 120, 2.1, 120 );
 // geoFloor.position.set( 0 , 0, 0 );
 // const matStdFloor = new THREE.MeshStandardMaterial( { color: 0x808080, roughness: 0.1, metalness: 0 } );
 // const mshStdFloor = new THREE.Mesh( geoFloor, matStdFloor );
@@ -61,67 +60,75 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 0, 0);
 controls.update();
 
-// const am=20;
-const am2=10;
 let cubes =[];
-const geometry = new THREE.SphereGeometry(1,10,10);
+const geometry = new THREE.SphereGeometry(1,20,20);
+// const geometry = new THREE.BoxGeometry();        
 
-let offset=0;
-for (let j = 0; j < am2; j++) {
-    let am=1+j*4;
-    for (let i = 0; i < am; i++) {
-        offset=i/am*TAU*4;//+j/am2*TAU*2;
-        // const geometry = new THREE.BoxGeometry();
-        // const material = new THREE.MeshBasicMaterial( { color: 0xffffff*Math.random() } );   
-        // const geometry = new THREE.ConeGeometry(.3,Math.random()*2);
-        let c;
-        if(i%2==0){
-            c = new THREE.Color(0x00ffff);
-        }
-        else{
-            c = new THREE.Color(0x000000);
-            
-        }
-        // const material = new THREE.MeshPhongMaterial( { color: c, shininess:200 } ); 
-        // const material = new THREE.MeshBasicMaterial( { color: c } );  
-        const material = new THREE.MeshPhysicalMaterial( { color: c, roughness: 1.1, metalness: 0 } );
 
-        // material.flatShading=true;
-        const cube = new THREE.Mesh(geometry, material);
-        cube.offset = offset;//i/am*TAU*4;
-        cube.s=j/am2;
-        // cube.position.x=Math.cos(i/am*TAU)*(j/am2*20);//*(j/am2*20+3);//Math.random()*10-5;
-        // cube.position.z=Math.sin(i/am*TAU)*(j/am2*20);//Math.random()*10-5;
-        let sc=30;
-        cube.position.y=sc/2-j/am2*sc;//Math.random()*10-5;
-        // cube.rotation.z = Math.random()*TAU;
-        // cube.rotation.x = Math.random()*TAU;
-        // cube.rotation.y = Math.random()*TAU;
-        cube.scale.y = cube.scale.x=cube.scale.z=noise.simplex2(i,i*2)*10;//c.s*3.5+Math.sin(t*21+c.offset)*1.5;
-
-        cubes.push(cube)
-        scene.add(cube);
+function addParticle(){
+    let c;
+    if(Math.random()>.5){
+        c = new THREE.Color(0x00ffff);
     }
+    else{
+        c = new THREE.Color(0xff0000);
+        
+    }
+    const material = new THREE.MeshBasicMaterial( { color: c } );  
+// material.flatShading=true;
+    const cube = new THREE.Mesh(geometry, material);
+    cube.life=1;
+    cube.rotateX(rr(-TAU,TAU));
+    cube.rotateY(rr(-TAU,TAU));
+    cube.rotateZ(rr(-TAU,TAU));
+    cube.lifedecay=.001+Math.random()*.005;
+    cube.userData.velocity=new THREE.Vector3(Math.random()-.5,Math.random()-.5,Math.random()-.5);
+    // cube.rotation.z = Math.random()*TAU;
+    // cube.rotation.x = Math.random()*TAU;
+    // cube.rotation.y = Math.random()*TAU;
+    cube.scale.y = cube.scale.x=cube.scale.z=1;
+    cube.seed = Math.random();
+    cubes.push(cube)
+    scene.add(cube);
 }
 
-
 // scene.add(a_light);
+let counter=0;
 function animate() {
 	requestAnimationFrame(animate);
-    let t = Date.now()*1
-    // light.position.x = Math.cos(t*TAU)*5;
-    // light.position.z = Math.sin(t*TAU)*5;
+    let t = Date.now()*0.01
+    if(counter<1100){
+        addParticle();
+        counter++;
+    }
     for (let i = 0; i < cubes.length; i++) {
         const c = cubes[i];
-        c.position.x+=noise.simplex2(c.position.x+t,c.position.z+i);
-        c.position.y+=noise.simplex2(c.position.y+t,c.position.z+i);
-        c.position.z+=noise.simplex2(c.position.y+t,c.position.z+i);
-        // c.rotation.x += 0.102221;
-        // c.scale.y = c.scale.x=c.scale.z=noise.simplex2(c.position.x+t,c.position.y);//c.s*3.5+Math.sin(t*21+c.offset)*1.5;
-        // console.log(i);
+        if(c.life>0){
+        c.scale.y = c.scale.x=c.scale.z=c.life*5+1.5+Math.sin(t*4*c.seed+c.seed)*1.5;
+        c.position.add(c.userData.velocity);
+        c.userData.velocity.multiplyScalar(.99);
+        c.life-=c.lifedecay;
+        if(Math.random()>.9){
+            var s = .5;
+            c.userData.velocity.add(new THREE.Vector3(rr(-s,s),rr(-s,s),rr(-s,s)));
+        }
     }
+    else{
+            // c.dispose();
+            // cubes.splice(i,1);
+            c.life=1;
+            c.position.set(0,0,0);
+            c.userData.velocity.set(Math.random()-.5,Math.random()-.5,Math.random()-.5);
+        }
+        // c.rotation.x += 0.102221;
+        // console.log(c.offset);
+    }
+    counter++;
     // controls.update();
 	renderer.render( scene, camera );
     // stats.update();
 }
 animate();
+function rr(min, max){
+    return min+Math.random()*(max-min);
+}
