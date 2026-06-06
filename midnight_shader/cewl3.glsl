b39295b651b9ea05abbcad13a4ac82b35f1bbcde
Sasha Tabakov <tequibo@gmail.com>
2026-06-07 02:46:43 +0300

    countwown shader

--- midnight_shader/cewl3.glsl
+++ midnight_shader/cewl3.glsl
@@ -14,10 +14,12 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord){
     float l = length(uv);
     uv = sin(uv*15.);
     float a2 = atan(uv.x, uv.y);
-    float t = fract(iTime*time_scale);
+    float t = fract(iTime*time_scale-sin(iTime*2.)*0.5*l+sin(iTime*6.)*l);
+    t = -sin(iTime*1.)*l*.5-sin(iTime*3.)*1.5*l-sin(iTime*0.5)*1.5*l;
+    t = sin(iTime*1.)*l*2.5;
     float c = 0.;
     float rad_time = t*TAU;
-    float r = 0.5 + 0.5 * cos(atan(uv.x, uv.y)-rad_time+l*-31.);
+    float r = 0.5 + 0.5 * cos(atan(uv.x, uv.y)-rad_time+l*-11.);
     // c = g;
     // r = r * 0.5;
     c = r;
